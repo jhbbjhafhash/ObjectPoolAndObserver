9061e63b3dc20c71dca31df7b311c06a2f42ea3b
jhbbjhafhash <katzn@my.erau.edu>
2025-09-25 21:02:02 -0700

    Scripts

---
+++ Scripts/BikeController.cs
@@ -0,0 +1,86 @@
+﻿using Chapter.ObjectPool;
+using UnityEngine;
+
+namespace Chapter.Observer
+{
+    public class BikeController : Subject
+    {
+        public bool IsTurboOn
+        {
+            get; private set;
+        }
+
+        public float CurrentHealth
+        {
+            get { return health; } 
+            
+        }
+        
+        private bool _isEngineOn;
+        private HUDController _hudController;
+        private DroneObjectPool _droneObjectPool;
+        private CameraController _cameraController;
+        [SerializeField] private float health = 100.0f;
+
+        void Awake()
+        {
+            _hudController =
+                gameObject.AddComponent<HUDController>();
+
+            _cameraController = FindAnyObjectByType<CameraController>();
+                
+            _droneObjectPool = FindAnyObjectByType<DroneObjectPool>();
+        }
+
+        private void Start()
+        {
+            StartEngine();
+        }
+
+        void OnEnable()
+        {
+            if (_hudController) 
+                Attach(_hudController);
+            
+            if (_cameraController) 
+                Attach(_cameraController);
+        }
+        
+        void OnDisable()
+        {
+            if (_hudController) 
+                Detach(_hudController);
+            
+            if (_cameraController) 
+                Detach(_cameraController);
+        }
+
+        private void StartEngine()
+        {
+            _isEngineOn = true;
+            
+            NotifyObservers();
+        }
+        
+        public void ToggleTurbo()
+        {
+            if (_isEngineOn) 
+                IsTurboOn = !IsTurboOn;
+            
+            NotifyObservers();
+        }
+        
+        public void TakeDamage(float amount)
+        {
+            health -= amount;
+            IsTurboOn = false;
+            
+            NotifyObservers();
+
+            _droneObjectPool.Spawn();
+            
+            if (health < 0)
+                Destroy(gameObject);
+        }
+    }
+}
---
+++ Scripts/ClientObserver.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Chapter.Observer
+{
+    public class ClientObserver : MonoBehaviour
+    {
+        private BikeController _bikeController;
+        
+        void Start()
+        {
+            _bikeController = FindAnyObjectByType<BikeController>();
+        }
+        
+        void OnGUI()
+        {
+            if (GUILayout.Button("Damage Bike"))
+                if (_bikeController) 
+                    _bikeController.TakeDamage(15.0f);
+
+            if (GUILayout.Button("Toggle Turbo"))
+                if (_bikeController) 
+                    _bikeController.ToggleTurbo();
+        }
+    }
+}
